1778b2de198976e53b6fc8fc6a4db49055a8b327
Emerson Xavier <meso.xavier@gmail.com>
2024-05-30 16:17:48 -0300

    "feat: atualizando imagens"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,10 @@ import { FaGithub } from 'react-icons/fa';
 const Navbar = () => {
   return <nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2" src={logo} alt="logo" />
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl"> 
-        <FaLinkedin />
-        <FaGithub />
+        <a href="https://www.linkedin.com/in/emersonxavier99/" target="_blank"><FaLinkedin /></a>
+        <a href="https://github.com/messoxavier" target="_blank"><FaGithub /></a>
     </div>
 
   </nav>
