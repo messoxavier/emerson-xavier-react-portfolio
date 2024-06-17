b1310ad54d577ed6ab966999940b3e8a201cfdb1
Emerson Xavier <meso.xavier@gmail.com>
2024-06-17 10:16:15 -0300

    "feat: atualizando imagens dos projetos"

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,8 @@
 import project1 from "../assets/projects/sistema-ehora.jpg";
+import project2 from "../assets/projects/Moodle.jpg";
 import project3 from "../assets/projects/meteora.png";
+import project4 from "../assets/projects/SISGER.jpg";
+import project5 from "../assets/projects/portfolio.jpg";
 
 export const HERO_CONTENT = `Sou um profissional dedicado e focado em desenvolver soluções. 
 Com mais de 2 anos de experiência, trabalhei com as linguagens JavaScript e PHP, aprimorei habilidades em front-end com o React, assim como habilidades em back-end com Node.js, Laravel, MySQL e MongoDB.
@@ -24,21 +27,21 @@ export const EXPERIENCES = [
 export const PROJECTS = [
   {
     title: "SISGER",
-    image: project1,
+    image: project4,
     description:
       "O serviço de Telegestão conecta e aproxima os profissionais e gestores de saúde, facilitando a implementação dos processos administrativos e operacionais. Utilizando a webconferência como principal estratégia de transformação digital desde o início de suas atividades, a Diretoria Geral de Telessaúde constituiu a Rede de Videocolaboração em Saúde (RVS), que se tornou um importante recurso para interação on-line entre gestores, médicos e demais profissionais de saúde no estado de Pernambuco. A gestão à distância é aperfeiçoada por meio das ferramentas de videocolaboração, como videoconferência, webconferência e streaming. O sistema anteriormente criado é simples e pode ser acessado por computador, smartphone ou tablet. Através de um sistema de agendamento, o solicitante pode realizar o agendamento dos serviços oferecidos e o foco deste projeto é justamente trazer melhorias e satisfazer as necessidades dos usuários. A Rede de Videocolaboração em Saúde (RVS) será substituída por um novo sistema, chamado Sistema de Gerenciamento de Reunião (SISGER), que trará um novo visual, melhorias e uma segurança maior.",
     technologies: ["JavaScript", "React", "Node.js", "MySQL"],
   },
   {
     title: "Estruturação de Ambiente Virtual de Aprendizagem",
-    image: project1,
+    image: project2,
     description:
       "Estruturação e implementação de Ambiente Virtual de Aprendizagem para Diretoria Geral de Telessaúde da Secretaria Estadual de Saúde de Pernambuco, através da plataforma Moodle.",
     technologies: ["PHP", "Moodle", "HTML", "MySQL"],
   },
   {
     title: "Portfólio Web",
-    image: project1,
+    image: project5,
     description:
       "Portfólio Web profissional utilizando as tecnologias mais atuais do mercado.",
     technologies: ["JavaScript", "React", "Tailwind CSS"],
